8e9a1cafbd61b75834d0d4090711c3c793c11839
jvitorn <joaoeuno2011@gmail.com>
2020-03-14 15:44:11 -0300

    aprendendo a utilizar diretrivas e separando do HTML

--- public/js/controllers/fotos-controllers.js
+++ public/js/controllers/fotos-controllers.js
@@ -3,7 +3,7 @@ angular.module('alurapic').controller('FotosController', function($scope,$http) 
     $scope.fotos = [];
     //criando uma promessa
     const promise = $http.get('/v1/fotos');
-    //caso for comprida ela ira retornar os dados de 'v1/fotos'
+    //caso for cumprida ela ira retornar os dados de 'v1/fotos'
     promise.then(function(success){
         $scope.fotos = success.data;
     }).catch(function(error){
--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,1 @@
-angular.module('alurapic',[]);
-
-//https://4.bp.blogspot.com/-E_9AxCqIXSA/TxSmRH79v1I/AAAAAAAAAAM/P_PXAOxDyss/s1600/300px-Akamaru.jpg
-
-//Cachorro Akamaru
+angular.module('alurapic', ['minhasDiretivas']);
