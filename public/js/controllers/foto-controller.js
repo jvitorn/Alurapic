ce7adf4c5fe0a4b79bb5b5179d85276b3528ecef
jvitorn <joaoeuno2011@gmail.com>
2020-03-22 16:19:48 -0300

    adicionando editar

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -5,22 +5,29 @@ angular.module('alurapic')
 		$scope.mensagem = '';
 		//verificando se foi passado algum parametro nesta rota
 		if($routeParams.fotoId){
-			let rotaFoto = $routeParams.fotoId;
 			//caso o parametro foi passado , ira mostrar os dados desta foto pela api
-			$http.get('v1/fotos/', rotaFoto)
+			$http.get('v1/fotos/'+ $routeParams.fotoId)
 			.success(function(foto){
 				$scope.foto = foto;
 			})
 			.error(function(error){
 				console.log(erro);
-				$scope.mensagem = 'Não foi possivel obter esta foto ' + rotaFoto + 'para editar';
+				$scope.mensagem = 'Não foi possivel obter esta foto ' + $routeParams.fotoId + 'para editar';
 			});
 		};
 
 		$scope.submeter = function() {
 
 			if ($scope.formulario.$valid) {
+				//se existe um id ela ira atualizar os dados
+				if($scope.foto._id){
+					$http.put('v1/fotos'+$scope.foto._id,$scope.foto)
+					.success(function(){
 
+					})
+					
+				}//caso nao tiver , ira cadastrar novos dados 
+				else{
 				$http.post('/v1/fotos', $scope.foto)
 				.success(function() {
 					$scope.foto = {};
@@ -31,6 +38,8 @@ angular.module('alurapic')
 					$scope.mensagem = 'Não foi possível cadastrar a foto';
 				})
 
+				}
+				
 			}
 		};
 	});
